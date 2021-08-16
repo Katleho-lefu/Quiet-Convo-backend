ac179f5e5a84369567b247edd4a34f31e9e15906
Victor Lefu <victor@thedigitalacademy.co.za>
2021-08-16 11:01:26 +0200

    setup ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CarModule } from './car/car.module';
+
 
 
 @Module({
-  imports: [CarModule, MongooseModule.forRoot('mongodb+srv://Victor:Katdarad18@learning.ozzgj.mongodb.net/CarsDB?retryWrites=true&w=majority')],
+  imports: [MongooseModule.forRoot('')],
   controllers: [AppController],
   providers: [AppService],
 })
