bed8a89a89e24acb1299b1bbf832a03f4ab2992a
Victor Lefu <victor@thedigitalacademy.co.za>
2021-08-19 13:47:46 +0200

    adding mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 
 
+
 @Module({
   imports: [JwtModule.register({
     secret: 'super-secret-cat',
